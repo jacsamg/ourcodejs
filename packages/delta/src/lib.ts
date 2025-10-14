7f172911dfb01194b79c437eba3d00c2ea39e9c1
Jacob Samuel G. <26469677+jacsamg@users.noreply.github.com>
2025-10-13 20:58:54 -0600

    delta: streamline DeltaRouter constructor and addRouteToTree method

--- packages/delta/src/lib.ts
+++ packages/delta/src/lib.ts
@@ -15,41 +15,45 @@ export class DeltaRouter {
   private readonly root: DeltaNode = new DeltaNode();
   public readonly children = this.root.children;
 
-  constructor(items: DeltaRouteSetup[]) {
-    for (const item of items) {
-      const segments: DeltaSegmentInfo[] = getSegments(item.path);
-      const resolver = item.resolver;
-      let currentNode = this.root;
-
-      for (const segment of segments) {
-        const segmentKey = segment.isParam ? SEGMENT_PARAM_KEY : segment.value;
-
-        if (!currentNode.children.has(segmentKey)) {
-          currentNode.children.set(segmentKey, new DeltaNode());
-        }
-
-        currentNode.segment = segment;
-        currentNode = currentNode.children.get(segmentKey)!;
-      }
-
-      if (resolver instanceof DeltaRouter) {
-        // For nested DeltaRouter, add its root's children to the current node's children
-        for (const [key, childNode] of resolver.children.entries()) {
-          if (currentNode.children.has(key)) {
-            throw new Error('Router already exists for this route!');
-          }
-
-          currentNode.children.set(key, childNode);
-        }
-      }
-
-      if (currentNode.handler !== null) {
-        throw new Error('Handler already exists for this route!');
-      }
-
-      currentNode.method = item.method;
-      currentNode.handler = <HandlerFn>resolver;
+  constructor(routes: DeltaRouteSetup[]) {
+    for (const route of routes) {
+      this.addRouteToTree(route);
     }
+  }
+
+  private addRouteToTree(route: DeltaRouteSetup): void {
+    const segments: DeltaSegmentInfo[] = getSegments(route.path);
+    const resolver = route.resolver;
+    let currentNode = this.root; // Start from the root node
+
+    for (const segment of segments) {
+      const segmentKey = segment.isParam ? SEGMENT_PARAM_KEY : segment.value;
+
+      if (!currentNode.children.has(segmentKey)) {
+        currentNode.children.set(segmentKey, new DeltaNode());
+      }
+
+      currentNode.segment = segment;
+      currentNode = currentNode.children.get(segmentKey)!; // Move to the child node
+    }
+
+    if (resolver instanceof DeltaRouter) {
+      // For nested DeltaRouter, add its root's children to the current node's children
+      for (const [key, childNode] of resolver.children.entries()) {
+        if (currentNode.children.has(key)) {
+          throw new Error('Router already exists for this route!');
+        }
+
+        currentNode.children.set(key, childNode);
+      }
+    }
+
+    if (currentNode.handler !== null) {
+      throw new Error('Handler already exists for this route!');
+    }
+
+    currentNode.method = route.method;
+    currentNode.handler = <HandlerFn>resolver;
   }
 
   public getRoute(method: string = '', path: string = ''): DeltaRoute | null {
